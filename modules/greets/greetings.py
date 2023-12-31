ad06107aa9295084783367bfac1ec35e712da316
Govinda <anzelix@gmail.com>
2023-12-31 04:57:42 -0600

    early morning update

---
+++ modules/greets/greetings.py
@@ -0,0 +1,8 @@
+def greet_english(name):
+    return f"Hello, {name}!"
+
+def greet_spanish(name):
+    return f"Hola, {name}!"
+
+def greet_nepali(name):
+    return f"Namaste, {name}!"
---
+++ modules/greets/main.py
@@ -0,0 +1,4 @@
+import greetings
+name = 'Alice'
+print(greetings.greet_english(name))
+print(greetings.greet_nepali(name))
